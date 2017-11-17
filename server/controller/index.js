449fe31e03b20e4b3a7e47ba5120473fb852a8aa
ouyangli <frog@ouyanglideMacBook-Air.local>
2017-11-17 10:53:32 +0800

    add format data method

--- server/controller/index.js
+++ server/controller/index.js
@@ -3,11 +3,22 @@ import axios from 'axios';
 import Index from '../../shared/index.jsx';
 import util from "../lib/util";
 
+var formatData = function(data){
+	let result = []
+	if(data && data.list.length){
+		data.list.forEach((item)=>{
+			result.push({title:item.title});
+		})
+	}
+
+	return {list:result}
+}
+
 var index = function(req,res,next){
  	axios.get('https://shop.hnmall.com/oto/category-ajax.html').then((rs)=>{
  		//console.log(rs.data)
  		if(rs.data){
-	 		let initialState = rs.data
+	 		let initialState = formatData(rs.data)
 	 		const markup = util.getMarkupByComponent(Index ,{
 		 		initialState
 		 	});
